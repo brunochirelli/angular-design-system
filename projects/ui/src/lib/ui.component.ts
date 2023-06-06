d80dcc7a9795e5eb7ae57c11c0d7311d7842362c
Bruno Chirelli <brunochirelli@gmail.com>
2023-06-06 14:13:19 +0100

    add ui library

---
+++ projects/ui/src/lib/ui.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brn-ui',
+  template: `
+    <p>
+      ui works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UiComponent {
+
+}
