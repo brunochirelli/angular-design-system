dbf634c92c5058976f10ff7e963dfa63e7b29e50
Bruno Chirelli <brunochirelli@gmail.com>
2023-06-07 12:15:10 +0100

    first try to add global css

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of ui
  */
 
-export * from './lib/ui.service';
+export * from './lib/services/ui.service';
 export * from './lib/ui.module';
 
 export * from './lib/components/button/button.component';
