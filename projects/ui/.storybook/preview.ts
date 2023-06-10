7e84456c52375c04605d462f3168326ce64b5333
Bruno Chirelli <brunochirelli@gmail.com>
2023-06-10 17:38:41 +0100

    RELEASE 0.1.1

--- projects/ui/.storybook/preview.ts
+++ projects/ui/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/angular';
 
+// import '../src/lib/styles/core.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
