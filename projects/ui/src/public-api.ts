81ec76b221eaa86db3f95214652277b4f6b4ed78
Bruno Chirelli <brunochirelli@gmail.com>
2023-07-01 08:06:12 -0300

    fix import path for public-api.ts

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/services/ui.service';
 export * from './lib/ui.module';
 
 export * from './lib/components/button/button.component';
+export * from './lib/components/card/card.module';
+export * from './lib/components/card/card.component';
